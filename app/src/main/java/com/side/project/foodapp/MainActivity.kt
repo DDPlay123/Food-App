47bedb0adfc75ec21ad2275a89bf291d73bdfd7b
DDPlay123 <a70325000@gmail.com>
2022-09-26 23:25:07 +0800

    build bottom navigation

--- app/src/main/java/com/side/project/foodapp/MainActivity.kt
+++ app/src/main/java/com/side/project/foodapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.side.project.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.side.project.foodapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+
+        init()
+    }
+
+    private fun init() {
+        val bottomNavigationView: BottomNavigationView = mainBinding.bottomNav
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
