29f3552886bb1004a213169ded588d5d2e5b50d2
DDPlay123 <a70325000@gmail.com>
2022-10-03 23:46:59 +0800

    adjust code

--- app/src/main/java/com/side/project/foodapp/ui/adapter/CategoryMealsAdapter.kt
+++ app/src/main/java/com/side/project/foodapp/ui/adapter/CategoryMealsAdapter.kt
@@ -2,11 +2,16 @@ package com.side.project.foodapp.ui.adapter
 
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import coil.request.ErrorResult
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.side.project.foodapp.data.model.MealsByCategory
 import com.side.project.foodapp.databinding.ItemMealBinding
+import com.side.project.foodapp.utils.logE
 
 class CategoryMealsAdapter : RecyclerView.Adapter<CategoryMealsAdapter.ViewHolder>() {
     private var mealsList = ArrayList<MealsByCategory>()
@@ -21,7 +26,21 @@ class CategoryMealsAdapter : RecyclerView.Adapter<CategoryMealsAdapter.ViewHolde
         ViewHolder(ItemMealBinding.inflate(LayoutInflater.from(parent.context), parent, false))
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.binding.imgMeal.load(mealsList[position].strMealThumb)
+        holder.binding.imgMeal.load(mealsList[position].strMealThumb) {
+            // 監聽
+            listener(
+                onStart = {
+                    holder.binding.imgPlaceholder.visibility = View.VISIBLE
+                },
+                onError = { _: ImageRequest, result: ErrorResult ->
+                    logE("RandomImage", result.throwable.message.toString())
+                    holder.binding.imgPlaceholder.visibility = View.VISIBLE
+                },
+                onSuccess = { _: ImageRequest, _: SuccessResult ->
+                    holder.binding.imgPlaceholder.visibility = View.GONE
+                }
+            )
+        }
         holder.binding.tvMeal.text = mealsList[position].strMeal
     }
 
